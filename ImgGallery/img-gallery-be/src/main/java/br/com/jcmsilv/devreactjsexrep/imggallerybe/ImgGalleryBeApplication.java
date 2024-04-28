a55ec3312daa3a054b233ac3367cce7ebdf9942a
Cristiano <jcristaino.c@gmail.com>
2024-04-27 21:58:37 -0300

    Versao inicial do backend

---
+++ ImgGallery/img-gallery-be/src/main/java/br/com/jcmsilv/devreactjsexrep/imggallerybe/ImgGalleryBeApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcmsilv.devreactjsexrep.imggallerybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgGalleryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgGalleryBeApplication.class, args);
+	}
+
+}
---
+++ ImgGallery/img-gallery-be/src/test/java/br/com/jcmsilv/devreactjsexrep/imggallerybe/ImgGalleryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcmsilv.devreactjsexrep.imggallerybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgGalleryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
